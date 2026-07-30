5a265af51bf6a2843aa30d694a24c85345ea4c22
twagirumukiza <38731174+twagirumukiza@users.noreply.github.com>
2026-07-30 16:08:57 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Configuration Firebase pour Yellow Dot
+// Remplacez ces valeurs par votre configuration Firebase Realtime Database
+export const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    databaseURL: "YOUR_DATABASE_URL",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
